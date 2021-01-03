9d44a283b98017ecd01232520cc7e9a861ce99bc
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-03 22:37:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,24 @@ import { fetchQuizQuestions } from './API';
 import QuestionCard from './components/QuestionCard';
 
 // Import Types
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
 
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 

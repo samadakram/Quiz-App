584d25709e2c513d8731f741f2b64fe5ab7981a6
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-04 00:44:58 +0500

    Update App.styles.ts

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,5 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import BGImage from './images/bg.jpg';
+import Image from './images/bg.jpg';
 
 export const GlobalStyle = createGlobalStyle`
     html { 
@@ -7,7 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        backgroud-image: url(${BGImage});
+        backgroud-image: url(${Image});
         background-size: cover;
         margin: 0;
         padding: 0 20px;

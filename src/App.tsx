4bccdcaf9c39eafe473fece6990161e192b75420
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-03 23:00:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -70,9 +70,11 @@ const App = () => {
           callback={checkAnswer}
         />
       )}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!loading && !gameOver && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }

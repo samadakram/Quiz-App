c25312dd7847ffaf72dff5c905907f2158f051c1
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-03 22:52:16 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,9 @@ const App = () => {
       {!gameOver ?
         <p className="score">Score:</p>
         : null}
-      <p>Loading Question...</p>
+      {loading ?
+        <p>Loading Question...</p>
+        : null}
       {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
